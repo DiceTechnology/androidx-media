dc1cb8c185fafe84e4437d2272076976c45a7669
Guoen Yong <guoen.yong@endeavorstreaming.com>
2024-11-26 15:25:28 +0800

    Migrate Dice androidx-media to 1.4.1

--- libraries/common/src/main/java/androidx/media3/common/text/TextShadowSpan.java
+++ libraries/common/src/main/java/androidx/media3/common/text/TextShadowSpan.java
@@ -1,14 +1,18 @@
 package androidx.media3.common.text;
 
+import android.os.Bundle;
 import android.text.TextPaint;
 import android.text.style.CharacterStyle;
 import androidx.annotation.Nullable;
+import androidx.media3.common.util.Util;
 
 /**
  * A span that contains the text shadow but does not modifies the text as we can't apply multiple
  * shadows with span, instead the {@link TextShadow} will be applied in the {@link SubtitlePainter}.
  */
 public class TextShadowSpan extends CharacterStyle {
+
+  private static final String FIELD_TEXT_SHADOW = Util.intToStringMaxRadix(0);
 
   @Nullable
   private TextShadow textShadow;
@@ -26,5 +30,19 @@ public class TextShadowSpan extends CharacterStyle {
   @Override
   public void updateDrawState(TextPaint tp) {
     // Do nothing.
+  }
+
+  public Bundle toBundle() {
+    Bundle bundle = new Bundle();
+    if (textShadow != null) {
+      bundle.putBundle(FIELD_TEXT_SHADOW, textShadow.toBundle());
+    }
+    return bundle;
+  }
+
+  public static TextShadowSpan fromBundle(Bundle bundle) {
+    @Nullable Bundle textShadowBundle = bundle.getBundle(FIELD_TEXT_SHADOW);
+    @Nullable TextShadow textShadow = textShadowBundle == null ? null : TextShadow.fromBundle(textShadowBundle);
+    return new TextShadowSpan(textShadow);
   }
 }
